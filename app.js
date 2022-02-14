7c88bcbc2efa0ffa22f30cc44482489759bebf58
Jame McNeilan <jamesmcneilan@protonmail.com>
2022-02-13 17:49:09 -0700

    Fixed prod. error (moved error handling under static route handling)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 const connect = require('./db/connection.js');
 const { notFound, errorFound } = require('./middleware/errorMW.js')
 dotenv.config();
@@ -17,17 +18,18 @@ app.use('/api/users', require('./routes/paths/users.js'));
 app.use('/api/photos', require('./routes/paths/photos.js'));
 app.get('/ping', (req, res) => { return res.send('pong'); });
 
-// Error handling middleware
-app.use(notFound);
-app.use(errorFound);
-
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+  console.log("Mode: Production");
   app.use(express.static('public/build'));
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
   });
 };
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorFound);
 
 // Start the server on the assigned port
 const port = process.env.PORT || 5000;
